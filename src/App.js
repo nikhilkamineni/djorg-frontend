80572c8ba4b9c3b8fcfc575bb7131021d8d378ca
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-07-11 22:21:26 -0400

    Implement Add Note functionality
    - Remove graphQL
    - Disable User and Tags
    - Cleaned up code
    - Improved comments

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
-import ApolloClient from 'apollo-boost';
-import gql from 'graphql-tag';
 
-const client = new ApolloClient({
-  uri: 'http://127.0.0.1:8000/graphql/'
-});
 
+/* STYLES */
 const AppStyles = {
-  background: '#999999'
+  background: '#999999',
+  height: '100vh',
+  width: '100%',
+  margin: '0px'
 };
 
 const noteListStyles = {
@@ -18,12 +17,15 @@ const noteListStyles = {
 };
 
 const noteStyles = {
+  background: '#888888',
   border: '1px solid black',
   margin: '3px',
   padding: '5px',
   width: '15%'
 };
 
+
+/* LOGIN Component */
 const Login = props => {
   const loginHandler = event => {
     event.preventDefault();
@@ -32,12 +34,9 @@ const Login = props => {
       password: event.target.password.value
     };
 
-    console.log(creds);
-
     axios
       .post('http://127.0.0.1:8000/api-token-auth/', creds)
       .then(res => {
-        console.log(res.data.token);
         localStorage.setItem('token', res.data.token);
         props.loginHandler();
       })
@@ -53,28 +52,97 @@ const Login = props => {
       <input type="submit" value="Login" />
     </form>
   );
-};
+}; 
 
+
+/* ADDNOTE Component Start */
+const AddNote = props => {
+  const saveNoteHandler = event => {
+    event.preventDefault();
+
+    let url = 'http://127.0.0.1:8000/api/notes/';
+
+    let newNote = {
+      title: event.target.title.value,
+      content: event.target.content.value
+      // tags: event.target.tags.value.split(',')
+    };
+
+    let config = {
+      headers: {
+        'Authorization': `Token ${localStorage.getItem('token')}`,
+      }
+    };
+    console.log(url, newNote, config)
+
+    axios
+      .post(url, newNote, config)
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  };
+
+  return (
+    <form
+      action=""
+      style={{ display: 'flex', flexDirection: 'column', width: '200px' }}
+      onSubmit={saveNoteHandler}
+    >
+      <fieldset>
+        Title: <br />
+        <input type="text" name="title" />
+        Content: <br />
+        <input type="text" name="content" />
+        { /*
+          Tags: <br />
+          <div style={{ fontSize: '0.6em' }}>
+            (enter tags you want to include seperated by comma)
+          </div>
+          {props.tags.map(tag => (
+            <mark
+              style={{ background: tag.color, color: 'white', fontSize: '0.6em' }}
+              key={tag.name}
+            >
+              {' ' + tag.name + ' '}
+            </mark>
+          ))}{' '}
+          */}
+        <br />
+        {/*<input type="text" name="tags" />*/}
+        <input
+          type="submit"
+          value="Save"
+          style={{ width: '30%', alignSelf: 'center' }}
+        />
+      </fieldset>
+    </form>
+  );
+}; // ADDNOTE Component End
+
+
+/* APP Component Start */
 class App extends Component {
   state = {
     notesREST: [],
-    notesGRAPHQL: [],
+    noteTags: [],
     authenticated: false,
     toggleGRAPHQL: false
   };
 
   loginHandler = () => {
     this.setState({ authenticated: true });
-    console.log(this.state);
     let config = {
       headers: { Authorization: `Token ${localStorage.getItem('token')}` }
     };
-    axios
+
+    axios    // Get Notes from REST API
       .get('http://127.0.0.1:8000/api/notes/', config)
       .then(res => this.setState({ notesREST: [...res.data] }))
-      .catch(err => {
-        console.log(err);
-      });
+      .catch(err => console.log(`Error getting Notes: ${err}`));
+    
+    // axios    // Get Tags from REST API
+    //   .get('http://127.0.0.1:8000/api/notetags/')
+    //   .then(res => this.setState({ noteTagsREST: [...res.data] }))
+    //   .catch(err => console.log(`Error getting Tags: ${err}`));
   };
 
   logoutHandler = () => {
@@ -83,53 +151,19 @@ class App extends Component {
   };
 
   componentDidMount() {
-    // if (this.state.authenticated) {
-    //   let config = {
-    //     headers: { Authorization: `Token ${localStorage.getItem('token')}` }
-    //   };
-    //   axios
-    //     .get('http://127.0.0.1:8000/api/notes/', config)
-    //     .then(res => this.setState({ notesREST: [...res.data] }))
-    //     .catch(err => {
-    //       console.log(err);
-    //     });
-    // }
-
-    // axios
-    //   .get('http://127.0.0.1:8000/api/notes/')
-    //   .then(res => this.setState({ notesREST: [...res.data] }))
-    //   .catch(err => {
-    //     console.log(err);
-    //   });
-
-    client
-      .query({
-        query: gql`
-          {
-            notes {
-              title
-              content
-              user {
-                username
-                email
-              }
-              tags {
-                name
-                color
-              }
-            }
-          }
-        `
-      })
-      .then(res => this.setState({ notesGRAPHQL: [...res.data.notes] }))
-      .catch(err => console.log(err));
+    if (localStorage.getItem('token')) this.loginHandler();
   }
 
   render() {
     return (
       <div className="App" style={AppStyles}>
         {this.state.authenticated ? (
-          <h3 onClick={this.logoutHandler}>Sign Out</h3>
+          <a
+            onClick={this.logoutHandler}
+            style={{ color: 'maroon', cursor: 'pointer' }}
+          >
+            Sign Out
+          </a>
         ) : (
           <Login
             loginHandler={this.loginHandler}
@@ -145,52 +179,41 @@ class App extends Component {
                 return (
                   <li key={i} style={noteStyles}>
                     <h3>{note.title}</h3>
-                    <h4>{note.user.username}</h4>
-                    <h4>{note.user.email}</h4>
+                    { /* <h4>{note.user.username}</h4> */ }
+                    { /* <h4>{note.user.email}</h4> */ }
                     <p>{note.content}</p>
-                    <h5>
-                      {note.tags.map((tag, i) => {
-                        return (
-                          <mark
+                    { /*
+                      <h5>
+                        {note.tags.map((tag, i) => {
+                          return (
+                            <mark
                             key={i}
                             style={{
                               backgroundColor: tag.color,
-                              color: 'white',
-                              padding: '3px'
+                                color: 'white',
+                                padding: '3px'
                             }}
                           >
                             {tag.name}
-                          </mark>
-                        );
-                      })}
-                    </h5>
+                            </mark>
+                          );
+                        })}
+                      </h5> 
+                    */ }
                   </li>
                 );
               })}
             </ul>
-          </div>
-        )}
 
-        {this.state.toggleGRAPHQL && (
-          <div class="Notes-GRAPHQL">
-            <h2>This is coming from the GRAPHQL API</h2>
-            <ul style={noteListStyles}>
-              {this.state.notesGRAPHQL.map((note, i) => {
-                return (
-                  <li key={i} style={noteStyles}>
-                    <h3>{note.title}</h3>
-                    <h4>{note.user.username}</h4>
-                    <h4>{note.user.email}</h4>
-                    <p>{note.content}</p>
-                  </li>
-                );
-              })}
-            </ul>
+            { /* this.state.noteTagsREST.length > 0 && 
+            (<AddNote tags={this.state.noteTagsREST} />) 
+            */ }
+            <AddNote tags={this.state.noteTags}/>
           </div>
         )}
       </div>
-    );
-  }
-}
+    ); //return ends
+  } // render function ends
+} // APP Component End
 
 export default App;
